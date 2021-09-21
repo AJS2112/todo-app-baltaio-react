d521f411ba40c4859ae9a7dec035373f552b43de
Antonio Sucre <antonio.sucre@indikatore.com>
2021-09-21 04:53:52 -0300

    Provider

--- src/contexts/TodoContext.tsx
+++ src/contexts/TodoContext.tsx
@@ -1,5 +1,6 @@
 import React, { createContext } from "react";
 import { TodoContextType } from "./TodoContextType";
+import { Todo } from '../models/Todo';
 
 export const TodoContext = createContext<TodoContextType>({
     todos: [],
@@ -7,3 +8,31 @@ export const TodoContext = createContext<TodoContextType>({
     removeTodo: () => { },
     toggle: () => { }
 });
+
+const TodoProvider = (props: any) => {
+
+    const todos: Todo[] = [
+        { id: 1, title: 'Ir ao supermercado', done: true },
+        { id: 2, title: 'Ir a academia', done: false }
+    ];
+
+    const addTodo = (title: string) => {
+        console.log('Adicionou ' + title);
+    }
+
+    const removeTodo = (todo: Todo) => {
+        console.log('Removeu ' + todo.title);
+    }
+
+    const toggle = (todo: Todo) => {
+        console.log('Alterou ' + todo.title);
+    }
+
+    return (
+        <TodoContext.Provider value={{ todos, addTodo, removeTodo, toggle }}>
+            {props.children}
+        </TodoContext.Provider>
+    );
+}
+
+export default TodoProvider;
