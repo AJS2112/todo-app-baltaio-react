6910e1bf3324dc3193c44218d0a6493f159cec86
Antonio Sucre <antonio.sucre@indikatore.com>
2021-09-22 20:13:01 -0300

    useEffect

--- src/contexts/TodoContext.tsx
+++ src/contexts/TodoContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import { TodoContextType } from "./TodoContextType";
 import { Todo } from '../models/Todo';
 import { get, save } from '../services/TodoService'
@@ -13,6 +13,10 @@ export const TodoContext = createContext<TodoContextType>({
 const TodoProvider = (props: any) => {
 
     const [todos, setTodos] = useState<Todo[]>(get);
+
+    useEffect(() => {
+        save(todos)
+    }, [todos]);
 
     const addTodo = (title: string) => {
         const todo: Todo = { id: todos.length + 1, title: title, done: false };
